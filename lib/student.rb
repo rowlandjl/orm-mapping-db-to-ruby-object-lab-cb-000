15da743e61eb06f21d59bba6eb78a2608976bf30
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-19 18:21:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -26,6 +26,8 @@ class Student
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    sql = <<-SQL
+    SQL
   end
   
   def save
