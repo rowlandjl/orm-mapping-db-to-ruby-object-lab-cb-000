e8f2f994131f9ca4e27dc3d6ce06cff055ac10cc
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-19 18:36:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -88,6 +88,10 @@ class Student
   
   def self.first_X_students_in_grade_10(number)
     sql = <<-SQL
+      SELECT *
+      FROM students 
+      WHERE grade = 10 
+      LIMIT ?
     SQL
   end 
   
